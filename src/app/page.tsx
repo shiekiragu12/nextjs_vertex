0c2ffb73a2c9e3a07e69492a1345c4c286af6b21
Joy Kiragu <sheerohkiragu@gmail.com>
2025-05-29 01:51:32 +0300

    updates on the auth login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+'use client'
+
+import React, { useState } from "react";
+import { getFunFactsAction } from "./actions";
 
 export default function Home() {
-return (
-    <main>
-        Hello World!
-    </main>
-);
+    const [animal, setAnimal] = useState('');
+    const [funFacts, setFunFacts] = useState<string[]>([]);
+    async function getNewFunFacts() {
+        const funFacts = await getFunFactsAction(animal);
+        setFunFacts(funFacts);
+    }
+    return (
+        <main>
+            <h1 className="text-xl">Animal Fun Facts!</h1>
+            <label className="m-2 p-2 text-lg">Animal</label>
+            <input
+                placeholder="dog"
+                value={animal}
+                onChange={(e) => setAnimal(e.target.value)}
+                className="m-2 p-2 border-2 rounded text-black"
+            />
+            <button
+                onClick={getNewFunFacts}
+                className="hover:bg-white m-2 p-2 border-2 rounded font-bold hover:text-black"
+            >
+                Get New Fun Facts
+            </button>
+            <ul className="list-disc list-inside">
+                {funFacts.map(function (thing) {
+                    return <li key={thing}>{thing}</li>
+                })}
+            </ul>
+        </main>
+    );
 }
